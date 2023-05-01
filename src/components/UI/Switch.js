7cd71aee1b61a359356d08e5e49312095a19dc42
Marek Zborowski <marek7654@gmail.com>
2023-05-01 19:33:30 +0200

    add UI Switch

---
+++ src/components/UI/Switch.js
@@ -0,0 +1,18 @@
+import classes from './Switch.module.css';
+
+const Switch = (props) => {
+  const {labelLeft, labelRight, absolute, value} = props;
+
+  return (
+    <div className={`${classes.switch_wrapper} ${absolute && classes.absolute}`}>
+      {labelLeft && <span className={classes.span_left}>{labelLeft}</span>}
+      <label className={classes.switch}>
+        <input className={classes.checkbox} type='checkbox' checked={value} onChange={props.onSwitchChange} />
+        <span className={classes.slider}></span>
+      </label>
+      {labelRight && <span className={classes.span_right}>{labelRight}</span>}
+    </div>
+  );
+};
+
+export default Switch;
